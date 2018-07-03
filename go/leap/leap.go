bde96ca421616d47bd023e2ee816754220008318
Dan Quan <daniel@quan.io>
2018-07-02 21:53:34 -0700

    Lint in addition to testing

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,5 +1,7 @@
+// Package leap provides a function to check if a year is a leap year
 package leap
 
+// IsLeapYear checks if a given year is a leap year
 func IsLeapYear(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
