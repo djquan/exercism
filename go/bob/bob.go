1663b78356935e6e0880e568abfce873c23ef6aa
Dan Quan <daniel@quan.io>
2018-07-02 21:16:49 -0700

    add bob

---
+++ go/bob/bob.go
@@ -0,0 +1,29 @@
+// Package bob provides a Hey method for responding as a lackadaisical teeneager
+package bob
+
+import (
+	"regexp"
+	"strings"
+)
+
+// Hey returns a teenager's response based on the original response
+func Hey(remark string) string {
+	trimmedRemark := strings.Trim(remark, " \t\n\r")
+	isQuestion := strings.HasSuffix(trimmedRemark, "?")
+	hasLetters, _ := regexp.MatchString("[A-z]", trimmedRemark)
+
+	switch {
+	case trimmedRemark == "":
+		return "Fine. Be that way!"
+	case hasLetters && strings.ToUpper(trimmedRemark) == trimmedRemark:
+		if isQuestion {
+			return "Calm down, I know what I'm doing!"
+		} else {
+			return "Whoa, chill out!"
+		}
+	case isQuestion:
+		return "Sure."
+	default:
+		return "Whatever."
+	}
+}
