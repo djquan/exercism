2885376de9571669ab8db28b0be1ddb3f3584468
Dan Quan <daniel@quan.io>
2018-07-06 13:00:49 -0700

    Extract into map

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,24 +2,30 @@
 package raindrops
 
 import (
+	"sort"
 	"strconv"
 	"strings"
 )
 
+var dropMap = map[int]string{
+	3: "Pling",
+	5: "Plang",
+	7: "Plong",
+}
+
 // Convert converts a number into raindrop sounds based on factors
 func Convert(i int) string {
 	var s strings.Builder
-
-	if i%3 == 0 {
-		s.WriteString("Pling")
+	var keys []int
+	for k := range dropMap {
+		keys = append(keys, k)
 	}
+	sort.Ints(keys)
 
-	if i%5 == 0 {
-		s.WriteString("Plang")
-	}
-
-	if i%7 == 0 {
-		s.WriteString("Plong")
+	for _, k := range keys {
+		if i%k == 0 {
+			s.WriteString(dropMap[k])
+		}
 	}
 
 	if s.Len() == 0 {
