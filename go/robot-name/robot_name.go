fab815f3b065f1e80971b2549c6bc04df355470e
Dan Quan <daniel@quan.io>
2018-07-19 06:58:21 -0700

    Add robot names

---
+++ go/robot-name/robot_name.go
@@ -0,0 +1,50 @@
+package robotname
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var names = make(map[string]bool)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+//Robot type represents a Robot with a name
+type Robot struct {
+	name string
+}
+
+/*Name generates a name for a robot if it does not have one.
+Returns it's name if it does have one.
+*/
+func (r *Robot) Name() string {
+	if r.name != "" {
+		return r.name
+	}
+
+	name := fmt.Sprintf("%c%c%d%d%d",
+		letters[rand.Intn(len(letters))],
+		letters[rand.Intn(len(letters))],
+		rand.Intn(9),
+		rand.Intn(9),
+		rand.Intn(9),
+	)
+
+	if names[name] == true {
+		return r.Name()
+	}
+
+	r.name = name
+	names[name] = true
+	return r.name
+}
+
+//Reset removes a robots old name and generates a new one.
+func (r *Robot) Reset() {
+	r.name = ""
+	r.Name()
+}
