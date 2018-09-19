7b44c9cea24a66adff08c92fdd33f7e7ceb7030f
Dan Quan <daniel@quan.io>
2018-09-18 19:35:30 -0700

    Feedback

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -13,15 +13,11 @@ var dropMap = map[int]string{
 	7: "Plong",
 }
 
+var keys = getKeys(dropMap)
+
 // Convert converts a number into raindrop sounds based on factors
 func Convert(i int) string {
 	var s strings.Builder
-	var keys []int
-	for k := range dropMap {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-
 	for _, k := range keys {
 		if i%k == 0 {
 			s.WriteString(dropMap[k])
@@ -29,7 +25,16 @@ func Convert(i int) string {
 	}
 
 	if s.Len() == 0 {
-		s.WriteString(strconv.FormatInt(int64(i), 10))
+		s.WriteString(strconv.Itoa(i))
 	}
 	return s.String()
+}
+
+func getKeys(m map[int]string) []int {
+	var keys []int
+	for k := range dropMap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	return keys
 }
