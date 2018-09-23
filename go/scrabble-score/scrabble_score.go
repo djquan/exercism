24aa72a89285684ae656316072aac57cc697363a
Dan Quan <daniel@quan.io>
2018-09-23 09:43:12 -0700

    Use switch case

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,23 +3,26 @@ package scrabble
 
 import "unicode"
 
-var letterScore = map[rune]int{
-	'A': 1, 'B': 3, 'C': 3,
-	'D': 2, 'E': 1, 'F': 4,
-	'G': 2, 'H': 4, 'I': 1,
-	'J': 8, 'K': 5, 'L': 1,
-	'M': 3, 'N': 1, 'O': 1,
-	'P': 3, 'Q': 10, 'R': 1,
-	'S': 1, 'T': 1, 'U': 1,
-	'V': 4, 'W': 4, 'X': 8,
-	'Y': 4, 'Z': 10,
-}
-
 // Score calculates a scrabble score of a given word
 func Score(w string) int {
 	s := 0
 	for _, l := range w {
-		s += letterScore[unicode.ToUpper(l)]
+		switch unicode.ToUpper(l) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			s++
+		case 'D', 'G':
+			s += 2
+		case 'B', 'C', 'M', 'P':
+			s += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			s += 4
+		case 'K':
+			s += 5
+		case 'J', 'X':
+			s += 8
+		case 'Q', 'Z':
+			s += 10
+		}
 	}
 	return s
 }
