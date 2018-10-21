acad87c731e50bc5af0fac9db553154233dae686
Dan Quan <daniel@quan.io>
2018-10-20 20:26:23 -0700

    Update pythagroem triplet

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,11 +1,9 @@
 pub fn find() -> Option<u32> {
-    for a in 1u32..1000 {
-        for b in a..(1000 - a) {
-            let c = 1000 - a - b;
-            if a.pow(2) + b.pow(2) == c.pow(2) {
-                return Some(a * b * c);
-            }
-        }
-    }
-    None
+    (1u32..1000)
+        .flat_map(|a| {
+            (a..1000 - a)
+                .map(move |b| (b, 1000 - a - b))
+                .filter(move |bc| a.pow(2) + bc.0.pow(2) == bc.1.pow(2))
+                .map(move |bc| a * bc.0 * bc.1)
+        }).nth(0)
 }
