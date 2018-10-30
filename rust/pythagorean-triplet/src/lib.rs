233743f6eabd3fa3412f25bd53934142ed4d890a
Dan Quan <daniel@quan.io>
2018-10-29 19:19:48 -0700

    Simplify pythagorean

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn find() -> Option<u32> {
     (1u32..1000)
-        .flat_map(|a| {
-            (a..1000 - a)
-                .map(move |b| (b, 1000 - a - b))
-                .filter(move |bc| a.pow(2) + bc.0.pow(2) == bc.1.pow(2))
-                .map(move |bc| a * bc.0 * bc.1)
-        }).nth(0)
+        .flat_map(|a| (a..1000 - a).map(move |b| (a, b, 1000 - a - b)))
+        .filter(|(a, b, c)| a.pow(2) + b.pow(2) == c.pow(2))
+        .map(|(a, b, c)| a * b * c)
+        .nth(0)
 }
