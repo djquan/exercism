6b414abc7bc4be0b6375594d939320a8fb6301f9
Dan Quan <daniel@quan.io>
2019-05-21 14:06:31 -0700

    WIP

---
+++ elixir/robot-simulator/robot_simulator.exs
@@ -0,0 +1,34 @@
+defmodule RobotSimulator do
+  @doc """
+  Create a Robot Simulator given an initial direction and position.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec create(direction :: atom, position :: {integer, integer}) :: any
+  def create(direction \\ :north, position \\ {0, 0}) do
+    %{position: position, direction: direction}
+  end
+
+  @doc """
+  Simulate the robot's movement given a string of instructions.
+
+  Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
+  """
+  @spec simulate(robot :: any, instructions :: String.t()) :: any
+  def simulate(robot, instructions) do
+  end
+
+  @doc """
+  Return the robot's direction.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec direction(robot :: any) :: atom
+  def direction(%{direction: dir}), do: dir
+
+  @doc """
+  Return the robot's position.
+  """
+  @spec position(robot :: any) :: {integer, integer}
+  def position(%{position: pos}), do: pos
+end
