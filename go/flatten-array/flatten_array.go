415a7f703cab7d14f7ce8511d8c703c31a77040f
Dan Quan <dan@quan.io>
2020-05-15 18:59:54 -0700

    refactor

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -2,10 +2,9 @@ package flatten
 
 //Flatten takes inputs of slices and flattens them
 func Flatten(input interface{}) []interface{} {
-	in := input.([]interface{})
 	result := make([]interface{}, 0)
 
-	for _, v := range in {
+	for _, v := range input.([]interface{}) {
 		if v != nil {
 			switch v.(type) {
 			case []interface{}:
