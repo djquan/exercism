b0a02da0c90a1f66f430b0b5dd12d758f1be0fc7
Dan Quan <dan@quan.io>
2020-05-19 08:02:11 -0700

    Use index semantics instead of append

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -2,10 +2,10 @@ package accumulate
 
 //Accumulate maps over a given input with a given function
 func Accumulate(given []string, converter func(string) string) []string {
-	result := make([]string, 0, len(given))
+	result := make([]string, len(given))
 
-	for _, val := range given {
-		result = append(result, converter(val))
+	for i, val := range given {
+		result[i] = converter(val)
 	}
 
 	return result
